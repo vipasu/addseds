0ec02445d80e708bc48f98168280dc44d4e49919
Vincent Su <vipasu@stanford.edu>
2016-08-10 23:15:25 -0700

    Comparing Henriques color-predicted color distribution

---
+++ henriques_color.py
@@ -0,0 +1,16 @@
+import util
+import calc
+import model
+import matplotlib.pyplot as plt
+
+
+data_dir = 'data/Henriques'
+cat = util.get_catalog("Henriques")
+dat = cat['dat']
+dat = util.load_proxies(dat, data_dir, ['s5'], ['s5'])
+df_train, df_test, _ = model.trainRegressor(dat, cat['box_size'])
+
+plt.hexbin(df_test['ssfr'], df_test['pred'])
+
+
+
